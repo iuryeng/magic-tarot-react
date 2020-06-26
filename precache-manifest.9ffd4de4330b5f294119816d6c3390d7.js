6a4b7fc4ca725f080467e1a7e33d4a228a7f5735
iury fernandes <iuryfernandes.eng@gmail.com>
2020-06-26 03:00:58 -0300

    Updates

---
+++ precache-manifest.9ffd4de4330b5f294119816d6c3390d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c11c065773e732990b214482c9c88c2",
+    "url": "/magic-tarot-react/index.html"
+  },
+  {
+    "revision": "002911b455c8e2794065",
+    "url": "/magic-tarot-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "47fabac427113882ccb7",
+    "url": "/magic-tarot-react/static/css/main.3e54d869.chunk.css"
+  },
+  {
+    "revision": "002911b455c8e2794065",
+    "url": "/magic-tarot-react/static/js/2.8cc90975.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic-tarot-react/static/js/2.8cc90975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fabac427113882ccb7",
+    "url": "/magic-tarot-react/static/js/main.0081b7e4.chunk.js"
+  },
+  {
+    "revision": "feb3f649e63e82d5b8b1",
+    "url": "/magic-tarot-react/static/js/runtime-main.f56dbf49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magic-tarot-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
